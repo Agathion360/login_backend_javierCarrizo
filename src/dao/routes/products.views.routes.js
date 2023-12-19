61fdb4994157f1cbfc119b7a23197d1041a8bd71
javier carrizo <carrizo38@gmail.com>
2023-12-18 21:45:12 -0300

    plantilla de login funcionando

--- src/dao/routes/products.views.routes.js
+++ src/dao/routes/products.views.routes.js
@@ -1,33 +1,46 @@
 import { Router } from 'express'
 import { ProductController } from '../controllers/product.controller.js'
+import {UserController} from '../controllers/user.controller.js'
 
 const router = Router()
 const controller = new ProductController()
+const userController = new UserController()
 
 
 router.get('/products', async (req, res) => {
-    const { limit, page, sort } = req.query;
-    
-    try {
-        const productsData = await controller.getProducts(limit, page, sort);
 
-        res.render('products', {
-            title: 'Listado de Productos',
-            rutaJs: 'products',
-            products: productsData.products,
-            totalProducts: productsData.total,
-            totalPages: productsData.pages,
-            currentPage: productsData.currentPage
-        });
-    } catch (err) {
-        res.status(500).render('error', {
-            message: 'Error al obtener productos',
-            error: { status: 500 }
-        });
+    const { limit, page, sort } = req.query;
+    if (req.session.user) {
+        try {
+            const productsData = await controller.getProducts(limit, page, sort);
+
+            res.render('products', {
+                title: 'Listado de Productos',
+                rutaJs: 'products',
+                products: productsData.products,
+                totalProducts: productsData.total,
+                totalPages: productsData.pages,
+                currentPage: productsData.currentPage
+            });
+        } catch (err) {
+            res.status(500).render('error', {
+                message: 'Error al obtener productos',
+                error: { status: 500 }
+            });
+        }
+    } else {
+        res.redirect('/login')
     }
 });
 
-
+router.get('/login', async (req, res) => {
+   
+    if (req.session.user) {
+        res.redirect('/profile')
+    } else {
+        res.render('login', { showNavbar: false })
+    }
+})
 
 
 
